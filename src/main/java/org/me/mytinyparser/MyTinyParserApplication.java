e544b9cdf712d9ccf744957a0e691913611e93d2
Rubin <lazarettorg@gmail.com>
2026-03-25 15:48:41 +0400

    Init commit

---
+++ src/main/java/org/me/mytinyparser/MyTinyParserApplication.java
@@ -0,0 +1,13 @@
+package org.me.mytinyparser;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyTinyParserApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MyTinyParserApplication.class, args);
+    }
+
+}
